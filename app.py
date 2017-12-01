13a6a7a8e13434ff814fcab755c1a9b5065d6213
Omar Quimbaya <omar.quimbaya4@gmail.com>
2017-12-01 17:28:48 -0600

    Adding new fields
    Need to figure out how best to accomplish my goal of making this a command line application.

--- app.py
+++ app.py
@@ -7,14 +7,17 @@ def get_organizations_from_zendesk(query):
     result_list = list()
     for org in zenpy_client.search(query, type="organization", sort_by="created_at", sort_order="desc"):
         result_list.append([
-            org.name, 
-            org.organization_fields.poc_start, 
+            org.name,
+            org.organization_fields.poc_start,
+            org.organization_fields.poc_expiration_date,
+            org.organization_fields.license_start_date,
             org.organization_fields.expiration,
             org.organization_fields.license_limit,
             org.organization_fields.type,
             org.organization_fields.location,
             org.organization_fields.partner,
-            org.notes
+            org.notes,
+            org.organization_fields.active_poc
         ])
         print("Adding {}".format(org.name))
     
@@ -49,14 +52,10 @@ if __name__ == '__main__':
 
     zenpy_client = Zenpy(token=api_token, subdomain=subdomain, email=email_addr)
 
-    HELP_TYPE = """
-    Type represents either a poc or orgs (all organizations). poc -- license_type:Trial -location:international -- We care about the trial license type that is not associated with an international account. orgs -- No query. This will pull everything from Zendesk and put it to a CSV for us.
-    """
-
     parser = argparse.ArgumentParser(
         description='Pull data about organizations via Zendesk and save data to a CSV.'
         )
-    parser.add_argument('type', type=str, help=HELP_TYPE)
+    parser.add_argument('type', type=str)
     args = parser.parse_args()
 
     csv_file = "poc.csv"
@@ -64,16 +63,20 @@ if __name__ == '__main__':
     header_row = [
         "Organization Name", 
         "POC Start", 
+        "POC End Date",
+        "License Start Date",
         "License Expiration", 
         "License Limit", 
         "Type", 
         "Location", 
         "Partner",
-        "Notes"
+        "Notes",
+        "POC T/F"
     ] 
 
     if args.type == "poc":
-        query = "license_type:Trial -location:international"
+        query = "active_poc:True -location:international"
+        # query = "active_poc:True"
         result_list = get_organizations_from_zendesk(query)  
         print("Number of POCs: {}".format(len(result_list)))
         write_csv(csv_file, header_row, result_list)
@@ -82,6 +85,5 @@ if __name__ == '__main__':
         result_list = get_organizations_from_zendesk(query) 
         print("Number of POCs: {}".format(len(result_list)))
         write_csv(all_orgs_csv_file, header_row, result_list)
-    elif args.type == "recent"
     else:
         print("You must choose poc or orgs.")
