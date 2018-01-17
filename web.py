1788dddc3a50c46fc231b1f7ef38bcff44cb3cae
Omar Quimbaya <omar.quimbaya4@gmail.com>
2018-01-16 20:46:44 -0600

    Removed app.py and moved over functions to web
    Using flask to display data on a webpage. Will need to work on other
    ways to query the dataset and give me the data I want.

--- web.py
+++ web.py
@@ -1,41 +1,60 @@
 from flask import Flask, render_template
 from zenpy import Zenpy
+import json
 import os
 
 
 app = Flask(__name__)
-api_token = os.environ['ZENDESK_API_KEY']
-subdomain = os.environ['ZENDESK_URL']
-email_addr = os.environ['ZENDESK_USER']
-zenpy_client = Zenpy(token=api_token, subdomain=subdomain, email=email_addr)
 
-def get_organizations_from_zendesk(query):
-    result_list = list()
-    for org in zenpy_client.search(query, type="organization", sort_by="created_at", sort_order="desc"):
-        result_list.append([
-            org.name, 
-            org.organization_fields.poc_start, 
-            org.organization_fields.expiration,
-            org.organization_fields.license_limit,
-            org.organization_fields.type,
-            org.organization_fields.location,
-            org.organization_fields.partner,
-            org.notes
-        ])
-    
-    for result in result_list:
+
+def get_env_data():
+    env = {
+        "token": os.environ['ZENDESK_API_KEY'],
+        "subdomain": os.environ['ZENDESK_URL'],
+        "email": os.environ['ZENDESK_USER'],
+    }
+    return env
+
+def get_client(token, subdomain, email):
+    client = Zenpy(token=token, subdomain=subdomain, email=email)
+    return client
+
+def format_results_dates(results):
+    for result in results:
         # Adjust the timestamps so they make sense in the CSV (hard-coded nastiness)
-        if result[1] is not None:
-            result[1] = result[1][0:10]
-        if result[2] is not None:
-            result[2] = result[2][0:10]
-    return result_list
+        if result["poc_start"] is not None:
+            result["poc_start"] = result["poc_start"][0:10]
+        if result["poc_expiration_date"] is not None:
+            result["poc_expiration_date"] = result["poc_expiration_date"][0:10]
+        if result["license_start_date"] is not None:
+            result["license_start_date"] = result["license_start_date"][0:10]
+        if result["expiration"] is not None:
+            result["expiration"] = result["expiration"][0:10]
+    return results
 
+def get_organizations(query=None):
+    client = get_client(**get_env_data())
+    result_list = list()
+    for org in client.search(query, type="organization", sort_order="desc"):
+        result_list.append({
+            "name": org.name,
+            "poc_start": org.organization_fields.poc_start,
+            "poc_expiration_date": org.organization_fields.poc_expiration_date,
+            "license_start_date": org.organization_fields.license_start_date,
+            "expiration": org.organization_fields.expiration,
+            "license_limit": org.organization_fields.license_limit,
+            "type": org.organization_fields.type,
+            "location": org.organization_fields.location,
+            "partner": org.organization_fields.partner,
+            "notes": org.notes,
+        })
+    formatted_results = format_results_dates(result_list)
+    return formatted_results
 
-results = get_organizations_from_zendesk("license_type:Trial -location:international")
 
 @app.route("/")
 def index():
+    results = get_organizations(query="active_poc:true -location:international")
     return render_template("index.html", results=results)
 
 
